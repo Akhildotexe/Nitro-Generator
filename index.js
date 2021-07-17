6a7c14f78bbfaab06917429806d404101ee6437b
Akhil Gosala <60161763+Akhildotexe@users.noreply.github.com>
2021-07-17 10:37:45 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const randomstring = require("randomstring");
+
+const mySecret = process.env['TOKEN']
+
+const prefix = '!';
+
+client.on('message', msg => {
+
+    let args = msg.content.slice(prefix.length).trim().split(' ');
+
+    if (msg.content.startsWith(`${prefix}setup`)) {
+
+        if (!msg.guild.member(client.user).hasPermission(["MANAGE_CHANNELS", "ADMINISTRATOR"])) return;
+        msg.guild.channels.create(`mining`, 'text').catch(e => { });
+
+    }
+
+    if (msg.content.startsWith(`${prefix}gen`)) {
+
+        msg.delete()
+
+        msg.channel.send("https://discord.gift/" + randomstring.generate(16));
+
+    }
+
+
+    if (msg.content.startsWith(`${prefix}start`)) {
+        if (!msg.guild.member(client.user).hasPermission(["ADMINISTRATOR"])) return;
+
+        msg.delete()
+
+        var interval = setInterval(function () {
+
+            msg.channel.send("https://discord.gift/" + randomstring.generate(16));
+
+        }, 2000);
+
+    }
+
+});
+
+client.on('ready', async () => {
+    console.log('was gud!')
+
+    let statuses = [
+        `free nitro`,
+        `free nitro`,
+        `${client.users.cache.size} Getting nitro👀`
+    ]
+
+    setInterval(function () {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        client.user.setActivity(status, {
+            type: "STREAMING",
+            url: "https://www.twitch.tv/..."
+        })
+    }, 4000)
+})
+
+client.login('ODQwMDE2NDcxMzY3MDkwMjI2.YJSETQ.b11iS8Oucl2NJvIKYh3lpqK8RdI')
+
